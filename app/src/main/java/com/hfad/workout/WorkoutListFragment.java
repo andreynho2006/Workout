b633401e32ad9a0678ad60bad97bbded5a4555eb
andreynho2006 <andreynho2006@gmail.com>
2018-12-11 18:19:58 +0200

    make an array to get all the names from the workout class and an adapter to set the list

--- app/src/main/java/com/hfad/workout/WorkoutListFragment.java
+++ app/src/main/java/com/hfad/workout/WorkoutListFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.hfad.workout.Workout;
 
 public class WorkoutListFragment extends ListFragment {
 
@@ -19,6 +22,14 @@ public class WorkoutListFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] names = new String[Workout.workouts.length];
+        for( int i = 0; i < names.length; i++) {
+            names[i] = Workout.workouts[i].getName();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(), android.R.layout.simple_list_item_1, names);
+        setListAdapter(adapter);
+
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
